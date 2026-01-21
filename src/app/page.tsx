a0910cb47517af5289a2cc733036be352fd3c3e9
muthiariesta <marsarchivee@gmail.com>
2026-01-21 14:04:08 +0700

    feat: add section after hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
-import Image from "next/image";import React from 'react'
+import Image from "next/image";
 import Link from "next/link";
+import { ArrowRight } from "lucide-react"; 
 
 const HomePage = () => {
   return (
@@ -37,6 +38,36 @@ const HomePage = () => {
                 className="bg-[#353B00] text-white font-montserrat text-sm md:text-base lg:text-lg font-bold px-6 md:px-8 py-3 md:py-4 rounded-full shadow-[0px_2px_20px_0px_rgba(0,0,0,0.25)] hover:brightness-110 transition-all"
               >
                 Infak & Zakat
+              </Link>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="bg-[#F7FFD8] p-4 sm:p-6">
+        <div 
+          className="max-w-348 mx-auto shadow-[0px_4px_32px_0px_rgba(0,0,0,0.25)] rounded-[32px] md:rounded-[48px] lg:rounded-[56px] p-6 sm:p-10 md:p-14 text-center"
+          style={{
+            background: 'linear-gradient(90deg, #ADCD61 0%, #FFFFFF 50%, #ADCD61 100%)',
+            boxShadow: 'inset 0px -4px 24px 0px rgba(255,255,255,0.5)'
+          }}
+        >
+          <div className="flex flex-col items-center gap-6 md:gap-10 lg:gap-12">
+            <h2 className="text-[#21272A] font-forum text-2xl sm:text-3xl md:text-4xl lg:text-[56px] font-normal leading-[66px]">
+              Banyak kegiatan yang kami buat, spesial untuk Anda
+            </h2>
+            <p className="text-[#21272A] font-montserrat text-sm sm:text-base md:text-lg lg:text-xl font-medium leading-[30px]">
+              Rhoncus morbi et augue nec, in id ullamcorper at sit. Condimentum sit nunc in eros scelerisque sed. Commodo in viverra nunc, ullamcorper ut. Non, amet, aliquet scelerisque nullam sagittis, pulvinar. Fermentum scelerisque sit consectetur hac mi. Mollis leo eleifend ultricies purus iaculis.
+            </p>
+            <div className="relative inline-block">
+               <Link
+                href="/timeline"
+                className="inline-flex items-center bg-[#FFC80B] text-[#353B00] font-montserrat text-sm md:text-base lg:text-lg font-bold pl-6 md:pl-10 pr-[3px] py-[3px] rounded-full shadow-[0px_4px_32px_0px_rgba(0,0,0,0.25)] hover:brightness-105 transition-all"
+              >
+                Jadwal Lengkap
+                <div className="ml-4 d:ml-6 w-[40px] h-[40px] md:w-[50px] md:h-[50px] text-white bg-[#9C4299] rounded-full flex items-center justify-center shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)] hover:brightness-105 transition-all">
+                  <ArrowRight size={20} strokeWidth={2.5} />
+                </div>
               </Link>
             </div>
           </div>
