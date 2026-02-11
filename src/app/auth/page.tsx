f628c25df57637c1f7c2b0d294404a01a8105c51
naufalzhafran <latifnaufal7@gmail.com>
2026-02-11 14:06:28 +0700

    feat: improve logged in user navbar

---
+++ src/app/auth/page.tsx
@@ -0,0 +1,9 @@
+import AuthForm from "@/components/auth/AuthForm";
+
+export default function AuthPage() {
+  return (
+    <div className="flex min-h-[calc(100vh-4rem)] items-center justify-center p-4">
+      <AuthForm />
+    </div>
+  );
+}
